e75f8d8fed825a1869d065cac9e2a2e108e44b10
khanhlam90 <91096296+khanhlam90@users.noreply.github.com>
2022-02-13 21:50:22 -0800

    user-model, create pizza

--- public/assets/js/add-pizza.js
+++ public/assets/js/add-pizza.js
@@ -51,6 +51,25 @@ const handlePizzaSubmit = event => {
   }
 
   const formData = { pizzaName, createdBy, size, toppings };
+
+  // add ability to actually POST that form data to the API
+  fetch('/api/pizzas', {
+    method: 'POST',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(formData)
+  })
+    .then(response => response.json())
+    .then(postResponse => {
+      alert('Pizza created successfully!');
+      console.log(postResponse);
+    })
+    .catch(err => {
+      console.log(err);
+    });
+
 };
 
 $pizzaForm.addEventListener('submit', handlePizzaSubmit);
